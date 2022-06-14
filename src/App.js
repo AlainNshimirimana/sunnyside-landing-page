5ce8966dad42baec63776132d7df4b86e3060796
alaindemain <alain.nshimirimana@gmail.com>
2022-06-13 19:50:25 -0700

    Client testimonials component created and imported and rendered to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from './components/Header';
 import BodyTop from './components/BodyTop';
+import Testimonials from "./components/Testimonials";
 import './css/App.css';
 
 function App(){
@@ -9,6 +10,7 @@ function App(){
         < div className='app-div'>
             <Header />
             <BodyTop />
+            <Testimonials />
         </div>
     )
 }
---
+++ src/components/Testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Testimonials.css';
+
+function Testimonials(){
+    return(
+        <div className='testimonials'>
+            <h3>Client Testimonials</h3>
+        </div>
+    )
+}
+
+export default Testimonials;
