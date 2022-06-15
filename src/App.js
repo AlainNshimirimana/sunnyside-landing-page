dca6046f3f6973a7ce51c63c6bfd263bc355bef0
alaindemain <alain.nshimirimana@gmail.com>
2022-06-14 20:42:27 -0700

    Image gallery completed and footer component is set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import BodyTop from './components/BodyTop';
 import Testimonials from "./components/Testimonials";
 import FooterImages from "./components/FooterImages";
+import Footer from "./components/Footer";
 import './css/App.css';
 
 function App(){
@@ -13,6 +14,7 @@ function App(){
             <BodyTop />
             <Testimonials />
             <FooterImages />
+            <Footer />
         </div>
     )
 }
